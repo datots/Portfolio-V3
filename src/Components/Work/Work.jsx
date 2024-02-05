b8ad6909fa68c64faf618bb355d7a06318ea4a25
datots <dato_ts@hotmail.com>
2024-02-05 23:54:13 +0400

    Updated portfolio

--- src/Components/Work/Work.jsx
+++ src/Components/Work/Work.jsx
@@ -4,7 +4,9 @@ import SQL from "../../Assets/SQL.png";
 import OOP from "../../Assets/OOP.webp";
 import Roman from "../../Assets/Roman.jpg";
 import form from "../../Assets/form.png";
-import investment_calculator from "../../Assets/investment_calculator.png"
+import hangman from "../../Assets/images.png";
+import coffee from "../../Assets/logo.png";
+import investment_calculator from "../../Assets/investment_calculator.png";
 
 import React from "react";
 
@@ -21,6 +23,65 @@ const Work = () => {
 
         {/* Grid Container */}
         <div className=" grid sm:grid-cols-2 md:grid-cols-3 gap-4w-64">
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${coffee})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Coffee House
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://github.com/datots/coffee-house"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 my-3 m-2 bg-white text-gray-700 text-bold text-lg">
+                    Code
+                  </button>
+                </a>
+                <a
+                  href="https://datots.github.io/coffee-house/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 my-3 m-2 bg-white text-gray-700 text-bold text-lg">
+                    Live
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${hangman})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                HangMan Game
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://github.com/datots/Hangman-game"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 my-3 m-2 bg-white text-gray-700 text-bold text-lg">
+                    Code
+                  </button>
+                </a>
+                <a
+                  href="https://datots.github.io/Hangman-game/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 my-3 m-2 bg-white text-gray-700 text-bold text-lg">
+                    Live
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
           {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${BMI})` }}
@@ -40,7 +101,10 @@ const Work = () => {
                     Code
                   </button>
                 </a>
-                <a href="https://bmi-calculator-hb89k22t5-dato-ts-hotmailcom.vercel.app/" target="_blank">
+                <a
+                  href="https://bmi-calculator-hb89k22t5-dato-ts-hotmailcom.vercel.app/"
+                  target="_blank"
+                >
                   <button className="text-center rounded-lg px-4 my-3 m-2 bg-white text-gray-700 text-bold text-lg">
                     Live
                   </button>
@@ -66,7 +130,10 @@ const Work = () => {
                     Code
                   </button>
                 </a>
-                <a href="https://investment-calculator-xi.vercel.app/" target="_blank">
+                <a
+                  href="https://investment-calculator-xi.vercel.app/"
+                  target="_blank"
+                >
                   <button className="text-center rounded-lg px-4 my-3 m-2 bg-white text-gray-700 text-bold text-lg">
                     Live
                   </button>
